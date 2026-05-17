5693ddb3153fd8f88cf2d87e01349d9a175801d9
SuspiciousApplePie <andreiusita6@gmail.com>
2026-05-17 18:46:30 +0800

    Fix file name

--- js/binarySearchTree.js
+++ js/binarySearchTree.js
@@ -3,15 +3,5 @@ function Node(data, left, right) {
 }
 
 function Tree(root) {
-  const buildTree = () => {};
-
-  const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null || node === undefined) {
-      return;
-    }
-
-    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-  };
+  const buildTree = (node) => {};
 }
